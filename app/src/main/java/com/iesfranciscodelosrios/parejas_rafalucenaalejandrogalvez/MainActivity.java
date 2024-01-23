d59754815989cd88bb604595761681ce0ea668f1
alexgg9 <alexgalvezgarciaqt@gmail.com>
2024-01-23 16:39:35 +0100

    DiseñoTerminado

--- app/src/main/java/com/iesfranciscodelosrios/parejas_rafalucenaalejandrogalvez/MainActivity.java
+++ app/src/main/java/com/iesfranciscodelosrios/parejas_rafalucenaalejandrogalvez/MainActivity.java
@@ -2,7 +2,9 @@ package com.iesfranciscodelosrios.parejas_rafalucenaalejandrogalvez;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.OrientationEventListener;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -17,16 +19,21 @@ public class MainActivity extends AppCompatActivity {
     private EditText keyEditText, valueEditText;
     private TextView resultTextView;
 
+    private OrientationEventListener orientationEventListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
+        getWindow().setSoftInputMode(android.view.WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
         sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
-        keyEditText = (EditText) findViewById(R.id.keyEditText);
-        valueEditText = (EditText) findViewById(R.id.valueEditText);
-        resultTextView = (TextView) findViewById(R.id.resultTextView);
+        keyEditText = findViewById(R.id.keyEditText);
+        valueEditText = findViewById(R.id.valueEditText);
+        resultTextView = findViewById(R.id.resultTextView);
     }
+
+
 
     public void saveData(View view) {
         String key = keyEditText.getText().toString().trim();
@@ -34,17 +41,14 @@ public class MainActivity extends AppCompatActivity {
 
         if (!key.isEmpty() && !value.isEmpty()) {
             if (sharedPreferences.contains(key)) {
-                // Key already exists, show a warning message
                 Toast.makeText(this, "Key already exists. Choose a different key or update the value.", Toast.LENGTH_SHORT).show();
             } else {
-                // Save the data
                 SharedPreferences.Editor editor = sharedPreferences.edit();
                 editor.putString(key, value);
                 editor.apply();
                 Toast.makeText(this, "Data saved successfully", Toast.LENGTH_SHORT).show();
             }
         } else {
-            // Empty key or value, show a warning message
             Toast.makeText(this, "Key and value cannot be empty", Toast.LENGTH_SHORT).show();
         }
     }
@@ -53,11 +57,10 @@ public class MainActivity extends AppCompatActivity {
         String key = keyEditText.getText().toString().trim();
 
         if (!key.isEmpty()) {
-            // Retrieve the value associated with the key
+
             String value = sharedPreferences.getString(key, "Key not found");
             resultTextView.setText(value);
         } else {
-            // Empty key, show a warning message
             Toast.makeText(this, "Enter a key to search", Toast.LENGTH_SHORT).show();
         }
     }
@@ -67,17 +70,14 @@ public class MainActivity extends AppCompatActivity {
 
         if (!key.isEmpty()) {
             if (sharedPreferences.contains(key)) {
-                // Key exists, delete the data
                 SharedPreferences.Editor editor = sharedPreferences.edit();
                 editor.remove(key);
                 editor.apply();
                 Toast.makeText(this, "Data deleted successfully", Toast.LENGTH_SHORT).show();
             } else {
-                // Key does not exist, show a warning message
                 Toast.makeText(this, "Key not found", Toast.LENGTH_SHORT).show();
             }
         } else {
-            // Empty key, show a warning message
             Toast.makeText(this, "Enter a key to delete", Toast.LENGTH_SHORT).show();
         }
     }
@@ -88,17 +88,14 @@ public class MainActivity extends AppCompatActivity {
 
         if (!key.isEmpty() && !value.isEmpty()) {
             if (sharedPreferences.contains(key)) {
-                // Key exists, update the value
                 SharedPreferences.Editor editor = sharedPreferences.edit();
                 editor.putString(key, value);
                 editor.apply();
                 Toast.makeText(this, "Data updated successfully", Toast.LENGTH_SHORT).show();
             } else {
-                // Key does not exist, show a warning message
                 Toast.makeText(this, "Key not found", Toast.LENGTH_SHORT).show();
             }
         } else {
-            // Empty key or value, show a warning message
             Toast.makeText(this, "Key and value cannot be empty", Toast.LENGTH_SHORT).show();
         }
     }
